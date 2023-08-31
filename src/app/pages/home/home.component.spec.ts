32661568231103f5d968b3227261b3bf3f0f497d
Eric Heintzmann <heintzmann.eric@free.fr>
2023-08-31 18:30:50 +0200

    #8 bug: Fix tests

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,6 +9,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
       declarations: [ HomeComponent ]
     })
     .compileComponents();
@@ -19,5 +23,12 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h2')?.textContent).toContain('Olympic games app');
   });
 });
